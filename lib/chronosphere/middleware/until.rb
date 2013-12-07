ab8fb52ebfe7b448e438ee654f0dc49a8499408f
Peter Souter <p.morsou@gmail.com>
2013-12-07 14:11:45 +0000

    Until and Since now limit date input Until only allows future dates, since only allows past dates

--- lib/chronosphere/middleware/until.rb
+++ lib/chronosphere/middleware/until.rb
@@ -9,6 +9,14 @@ module Chronosphere
 
         parsed_date = Chronic.parse(cli_date)
 
+        parsed_date = Date.parse(parsed_date.to_s)
+
+        if (parsed_date < Date.today)
+          say "You've asked for days until a date in the past!", :red
+          say "Use `chronosphere since '#{cli_date}' instead`", :green
+          exit 1
+        end
+
         days_since = (Date.parse(parsed_date.to_s) - Date.today).to_i
 
         say "It'll be #{days_since} days until #{cli_date}"
--- spec/cli/since_spec.rb
+++ spec/cli/since_spec.rb
@@ -13,6 +13,11 @@ describe Chronosphere::CLI do
   end
 
   describe "since" do
+    it "returns error string if you give a date in the future" do
+      expect{@cli.since('next year')}.to raise_error(SystemExit)
+      expect($stdout.string).to eq "You've asked for days since a date in the future!\nUse `chronosphere until 'next year' instead`\n"
+    end
+
     it "returns the amount of days until a date" do
       @cli.since('a year ago')
       expect($stdout.string).to eq "It's been 365 days since a year ago\n"
